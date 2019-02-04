f18a8c6af7555379221f69064669e23b452726e7
Juan Gonzalez <juan@sancrisoft.com>
2019-02-03 19:10:24 -0500

    Adding my info

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-It's me, Tierney!
+A personal card based on Tierney Cyren ([@bitandbang](https://twitter.com/bitandbang)) npx cards project. See https://github.com/bnb/bitandbang
 
 # Usage
 Via npx:
 ```
-npx bitandbang
+npx juanjo4
 ```
 
-Not including how to use it globally because I'm not sure why you'd want this as a global command. That'd be creepy.
+All credits to [@bnb](https://github.com/bnb)
--- build.js
+++ build.js
@@ -15,17 +15,19 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('               Tierney Cyren'),
-  handle: chalk.white('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  opensource: chalk.white('Node.js Community Committee ') + chalk.green('⬢'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('bitandbang'),
-  npm: chalk.gray('https://npmjs.com/') + chalk.red('~bnb'),
-  github: chalk.gray('https://github.com/') + chalk.green('bnb'),
-  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.red('npx') + ' ' + chalk.white('bitandbang'),
+  name: chalk.white('               Juan Gonzalez'),
+  handle: chalk.white('@juanjo4'),
+  work: chalk.white('Project Leader & Co-founder'),
+  workplace: chalk.keyword('orange')('@sancrisoft 🚀'),
+  opensource: chalk.white('[] ') + chalk.green('⬢'),
+  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('juanjo4'),
+  npm: chalk.gray('https://npmjs.com/') + chalk.red('~juanjo4'),
+  github: chalk.gray('https://github.com/') + chalk.green('juanjo4'),
+  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('juanjgonzalezp'),
+  web: chalk.cyan('TBD'),
+  npx: chalk.red('npx') + ' ' + chalk.white('juanjo4'),
   labelWork: chalk.white.bold('       Work:'),
+  labelWorkplace: chalk.white.bold('         At:'),
   labelOpenSource: chalk.white.bold('Open Source:'),
   labelTwitter: chalk.white.bold('    Twitter:'),
   labelnpm: chalk.white.bold('        npm:'),
@@ -39,6 +41,7 @@ const data = {
 const newline = '\n'
 const heading = `${data.name} / ${data.handle}`
 const working = `${data.labelWork}  ${data.work}`
+const workplace = `${data.labelWorkplace}  ${data.workplace}`
 const opensourcing = `${data.labelOpenSource}  ${data.opensource}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
 const npming = `${data.labelnpm}  ${data.npm}`
@@ -51,12 +54,13 @@ const carding = `${data.labelCard}  ${data.npx}`
 const output = heading + // data.name + data.handle
                newline + newline + // Add one whole blank line
                working + newline + // data.labelWork + data.work
-               opensourcing + newline + newline + // data.labelOpenSource + data.opensource
-               twittering + newline + // data.labelTwitter + data.twitter
+               workplace + newline + // space + data.workplace
+               //opensourcing + newline + newline + // data.labelOpenSource + data.opensource
+               newline + newline + twittering + newline + // data.labelTwitter + data.twitter
                npming + newline + // data.labelnpm + data.npm
                githubing + newline + // data.labelGitHub + data.github
                linkedining + newline + // data.labelLinkedIn + data.linkedin
-               webing + newline + newline + // data.labelWeb + data.web
-               carding // data.labelCard + data.npx
+               //webing + newline + newline + // data.labelWeb + data.web
+               newline + newline + carding // data.labelCard + data.npx
 
-fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
+fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.keyword('orange')(boxen(output, options)))
